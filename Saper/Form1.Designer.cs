50e16a0f657863e4c5e6de9887e1a10a03395445
ArtMoc <tem.mochalov@yandex.ru>
2022-03-31 12:51:28 +0300

    Add project files.

---
+++ Saper/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace Saper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.играToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.новаяИграToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.новичокToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 28);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(378, 389);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.играToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(378, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // играToolStripMenuItem
+            // 
+            this.играToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.новаяИграToolStripMenuItem});
+            this.играToolStripMenuItem.Name = "играToolStripMenuItem";
+            this.играToolStripMenuItem.Size = new System.Drawing.Size(57, 24);
+            this.играToolStripMenuItem.Text = "Игра";
+            // 
+            // новаяИграToolStripMenuItem
+            // 
+            this.новаяИграToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.новичокToolStripMenuItem});
+            this.новаяИграToolStripMenuItem.Name = "новаяИграToolStripMenuItem";
+            this.новаяИграToolStripMenuItem.Size = new System.Drawing.Size(172, 26);
+            this.новаяИграToolStripMenuItem.Text = "Новая игра";
+            // 
+            // новичокToolStripMenuItem
+            // 
+            this.новичокToolStripMenuItem.Name = "новичокToolStripMenuItem";
+            this.новичокToolStripMenuItem.Size = new System.Drawing.Size(153, 26);
+            this.новичокToolStripMenuItem.Text = "Новичок";
+            this.новичокToolStripMenuItem.Click += new System.EventHandler(this.новичокToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 417);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem играToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem новаяИграToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem новичокToolStripMenuItem;
+    }
+}
+
---
+++ Saper/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Saper
+{
+    public partial class Form1 : Form
+    {
+        SapperGame sapperGame;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        void RefreshGrid()
+        {
+            dataGridView1.ColumnCount = 50;
+            dataGridView1.RowCount = 50;
+            for (int i = 0; i < sapperGame.N; i++)
+            {
+                dataGridView1.Rows[i].Height = 30;
+                dataGridView1.Columns[i].Width = 30;
+                /*Height = 50 * sapperGame.N + 100;//форма становится под размер поля
+            Width = 50 * sapperGame.N + 70;*/
+            }
+        }
+
+        void ShowData()
+        {
+            for (int col = 0; col < sapperGame.N; col++)
+            {
+                for (int row = 0; row < sapperGame.N; row++)
+                    dataGridView1[col, row].Value = sapperGame.Area[row, col].ToString();
+            }
+        }
+
+        private void новичокToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            sapperGame = new SapperGame(10, 5);
+            RefreshGrid();
+            NewGame();
+            // ShowData();
+        }
+
+        void NewGame()
+        {
+            for (int col = 0; col < sapperGame.N; col++)
+            {
+                for (int row = 0; row < sapperGame.N; row++)
+                {
+                    dataGridView1[col, row].Style.BackColor = Color.White;
+                    dataGridView1[col, row].Value = "";
+                }
+            }
+        }
+
+        void DrawZero(int col, int row)
+        {
+            if (col < 0 || row < 0 || col > sapperGame.N - 1 || row > sapperGame.N - 1) return;
+            if (sapperGame.Area[row, col] != 0)
+            {
+                dataGridView1[col, row].Style.BackColor = Color.Yellow;
+                dataGridView1[col, row].Value = sapperGame.Area[row, col].ToString();
+                return;
+            }
+            if (dataGridView1[col, row].Style.BackColor == Color.Yellow ||
+                dataGridView1[col, row].Style.BackColor == Color.LightSeaGreen) return;
+
+            dataGridView1[col, row].Style.BackColor = Color.LightSeaGreen;
+
+            DrawZero(col + 1, row);
+            DrawZero(col - 1, row);
+
+            DrawZero(col - 1, row - 1);
+            DrawZero(col + 1, row - 1);
+            DrawZero(col, row - 1);
+
+            DrawZero(col - 1, row + 1);
+            DrawZero(col + 1, row + 1);
+            DrawZero(col, row + 1);
+        }
+
+        bool Winner()
+        {
+            int count = 0;
+            for (int col = 0; col < sapperGame.N; col++)
+
+                for (int row = 0; row < sapperGame.N; row++)
+                {
+                    if (dataGridView1[col, row].Style.BackColor == Color.White)
+                    {
+                        count++;
+                    }
+                }
+            return count == sapperGame.MinesCount;
+        }
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (sapperGame.IsEnd == false)
+            {
+                int col = e.ColumnIndex;
+                int row = e.RowIndex;
+                if (sapperGame.Area[row, col] > 10)
+                {
+                    dataGridView1[col, row].Style.BackColor = Color.Red;
+                    sapperGame.IsEnd = true;
+                    MessageBox.Show("Вы подорвались на мине");
+                }
+                else if (sapperGame.Area[row, col] != 0)
+                {
+                    dataGridView1[col, row].Style.BackColor = Color.Yellow;
+                    dataGridView1[col, row].Value = sapperGame.Area[row, col].ToString();
+                }
+                else
+                {
+                    DrawZero(col, row);
+                }
+                if (!sapperGame.IsEnd && Winner())
+                {
+                    MessageBox.Show("Победа");
+                    sapperGame.IsEnd = true;
+                }
+            }
+            dataGridView1.ClearSelection();
+        }
+    }
+}
